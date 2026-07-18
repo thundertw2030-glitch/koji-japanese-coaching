8ccefd3e791fb230a0786d8c6316ef33a910c1b2
thundertw2030-glitch <thundertw2030@gmail.com>
2026-07-19 08:57:40 +0900

    Add files via upload

---
+++ assets/config.v3.1.js
@@ -0,0 +1,11 @@
+window.KOJI_CONFIG = {
+  siteUrl: "https://nimble-shortbread-ea1a37.netlify.app",
+  email: "thundertw2030@gmail.com",
+  whatsappUrl: "https://wa.me/819030562663",
+  whatsappDisplay: "+81 90-3056-2663",
+  lineUrl: "https://line.me/ti/p/GfZmB69ify",
+  gaMeasurementId: "G-GWQ16V9P2J",
+  clarityProjectId: "xoeg2xrf8y",
+  brandVersion: "3.0",
+  introVideoEnabled: false
+};
